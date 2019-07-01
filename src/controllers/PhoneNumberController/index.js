9770f923ceaf1fa3dc842655310a93c84751dd5d
obulaworld <obulaworldofexcellence@gmail.com>
2019-07-01 12:24:30 +0100

    ch(readme) update readme

--- src/controllers/PhoneNumberController/index.js
+++ src/controllers/PhoneNumberController/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from './node_modules/express';
 import PhoneNumberController from './PhoneNumberController';
 import QueryValidator from '../../middlewares/checkQuery';
 
